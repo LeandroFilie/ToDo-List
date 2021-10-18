49a2ce450b79c65739ad322d7dfd617262d99e06
LeandroFilie <leandro.gf03@gmail.com>
2021-10-18 20:44:28 -0300

    Add menu mobile

---
+++ script.js
@@ -0,0 +1,25 @@
+function getDate(){
+  const currentDate = new Date;
+  const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maior', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+
+  const day = currentDate.getDate();
+  const month = months[currentDate.getMonth()];
+  const year = currentDate.getFullYear();
+  const weekDay = weekDays[currentDate.getDay()];
+
+  document.getElementById('currentDate').innerHTML = `${weekDay}, ${day} de ${month} de ${year}`;
+}
+
+getDate();
+
+const buttonMenu = document.getElementById('open-menu');
+const menu = document.getElementById('menu')
+
+function toggleClass(){
+  buttonMenu.classList.toggle('active');
+  menu.classList.toggle('active');
+
+}
+
+buttonMenu.addEventListener('click',toggleClass);
